1c9ba443cecb4a82118063e6f8c426e66152ebe3
sepehr mir <sepehr.khabaloo@yahoo.com>
2023-04-16 15:41:23 +0330

    using useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,67 +1,49 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Card from "./components/Card/Card";
 import classes from "./App.module.css";
 import Input from "./components/Input/Input";
 import ItemsList from "./components/items/ItemsList";
-import { save, clear, parsed, remove } from "./functions/functions.js";
+import { parsed } from "./functions/functions.js";
+import itemsReducer from "./components/items/itemsReducer";
+import { clear } from "./functions/functions";
 
 function App() {
-  const [itemsList, setItemsList] = useState([]);
-  const [counter, setCounter] = useState(0);
-  const [itemDeleted, setItemDeleted] = useState(false);
+  const [items, itemsDispatch] = useReducer(itemsReducer, {
+    items: [],
+    counter: 0,
+  });
 
-  const newItemHandler = (item) => {
-    let newArr = [];
-    setItemsList((prevItemsList) => {
-      newArr = [...prevItemsList, { text: item, id: counter }];
-      return newArr;
-    });
-
-    clear();
-    save("array", newArr);
-    if (newArr.length != 0) {
-      save("lastId", newArr[newArr.length - 1].id);
-    }
-    setCounter(counter + 1);
+  const newItemHandler = (text) => {
+    itemsDispatch({ type: "ADD_ONE", newText: text });
   };
 
   useEffect(() => {
     const parsedArray = parsed("array");
-    const parsedCounter = parsed("lastId");
 
-    if (parsedArray && parsedArray.length != 0 && counter == 0) {
-      setItemsList(parsedArray);
-      setCounter(parsedCounter + 1);
-    } else if (itemDeleted == true) {
-      remove("array");
-      save("array", itemsList);
+    if (parsedArray && parsedArray.length !== 0 && items.items.length === 0) {
+      itemsDispatch({
+        type: "ADD_OLD",
+      });
     }
-    setItemDeleted(false);
-  }, [itemsList]);
+  }, []);
 
   const clearBtnHandler = (e) => {
     clear();
-    setCounter(0);
-    setItemsList([]);
+    itemsDispatch({ type: "CLEAR" });
   };
 
   const onDeleteItemHandler = (id) => {
-    console.log(itemsList);
-
-    setItemsList((prevItemList) => {
-      return prevItemList.filter((item) => item.id != id);
-    });
-    setItemDeleted(true);
+    itemsDispatch({ type: "DELETE_ONE", id: id });
   };
 
   return (
     <Card className={classes.mainCard}>
       <Input onItem={newItemHandler} />
-      {itemsList.length != 0 && (
-        <ItemsList items={itemsList} onDeleteItemId={onDeleteItemHandler} />
+      {items.items.length !== 0 && (
+        <ItemsList items={items.items} onDeleteItemId={onDeleteItemHandler} />
       )}
       <div className={classes.actions}>
-        {itemsList.length != 0 && (
+        {items.items.length !== 0 && (
           <button onClick={clearBtnHandler}>clear List</button>
         )}
       </div>
---
+++ src/components/items/itemsReducer.js
@@ -0,0 +1,55 @@
+import { save, clear, parsed, remove } from "../../functions/functions";
+
+const itemsReducer = (state, action) => {
+  if (action.type === "ADD_ONE") {
+    const newArr = [
+      ...state.items,
+      { text: action.newText, id: state.counter },
+    ];
+    clear();
+    save("array", newArr);
+    if (newArr.length !== 0) {
+      save("lastId", newArr[newArr.length - 1].id);
+    }
+
+    return {
+      items: newArr,
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "ADD_OLD") {
+    return {
+      items: parsed("array"),
+      counter: parsed("lastId") + 1,
+    };
+  }
+
+  if (action.type === "CLEAR ") {
+    clear();
+
+    return {
+      items: [],
+      counter: 0,
+    };
+  }
+
+  if (action.type === "DELETE_ONE") {
+    const newArr = state.items.filter((item) => item.id !== action.id);
+
+    remove("array");
+    save("array", newArr);
+    if (newArr.length === 0) {
+      clear();
+    }
+
+    return {
+      items: newArr,
+      counter: state.counter,
+    };
+  }
+
+  return { items: [], counter: 0 };
+};
+
+export default itemsReducer;
